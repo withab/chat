50d3d91d82dfa6d4a67aa3efe1ed058e49a307d0
withan <39221862+withab@users.noreply.github.com>
2021-07-05 22:20:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} Connection`)
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} Disconnection`)
+    });
+    socket.on('message', (message) => {
+        console.log(`New message:\nAuthor: ${message.author}\nContent: ${message.content}\nDate: ${new Date(message.date)}`)
+        io.emit('message', message)
+    })
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
