bad346ea458667ead6d0f3e7b4b1dc5ba350fa09
withan <39221862+withab@users.noreply.github.com>
2021-07-05 22:46:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -20,6 +22,6 @@ io.on('connection', (socket) => {
     })
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
